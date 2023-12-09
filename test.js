ae94e4fd2b848556569fead37b18abcfcbd408ab
Dmitrii <aeroformat0728@gmail.com>
2023-12-10 00:17:35 +0300

    Update

---
+++ test.js
@@ -0,0 +1,4 @@
+document.getElementById('transitionButton').addEventListener('click', function() {
+    // Используйте window.location.href для перехода на другую страницу
+    window.location.href = 'another-page.html';
+});
