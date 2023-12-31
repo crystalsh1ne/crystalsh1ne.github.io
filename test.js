923bd9a58b89f8a4492d69ba39739f5a4f29555d
Dmitrii <aeroformat0728@gmail.com>
2023-12-31 19:05:24 +0300

    Обновление сайта
    Обновил полностью сайт.

--- test.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('transitionButton').addEventListener('click', function() {
-    // Используйте window.location.href для перехода на другую страницу
-    window.location.href = 'another-page.html';
-});
